bcac7c5d79d444c59dd6c07d50e0a45e58c1e70c
zhoulianyi <zhoulianyi@antrol.com>
2015-11-25 15:00:31 +0800

    react的redux版本,es5模式的

--- containers/App.js
+++ containers/App.js
@@ -1,16 +1,13 @@
 var React = require('react');
 var TodoApp = require('./TodoApp');
-var createRedux = require('redux').createRedux;
-var Provider = require('redux/react').Provider;
-var stores = require('../stores');
-
-var redux = createRedux(stores);
+var Provider=require("react-redux").Provider;
+var store = require('../stores/todos');
 
 var App = React.createClass({
   render: function() {
     return (
-      <Provider redux={redux}>
-        {function() { return <TodoApp />; }}
+      <Provider store={store}>
+        <TodoApp />
       </Provider>
     );
   }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+var ReactDOM = require('react-dom');
 var React = require('react');
 var App = require('./containers/App');
 require('todomvc-app-css/index.css');
 
-React.render(
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
